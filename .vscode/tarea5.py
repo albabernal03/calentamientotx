96c130fb8f9cb771de8a8c83cc5341c13386c4b1
albabernal03 <ALBABERO@MYUAX.COM>
2021-10-30 20:08:16 +0200

    tarea6 completada

--- .vscode/tarea5.py
+++ .vscode/tarea5.py
@@ -1,7 +1,7 @@
 dinero = 2000
 precio_helado = 100
-hambre = edad
 edad = 18
+hambre = edad
 helados_consumidos = 0
 
 while (hambre) < 85 and (dinero - precio_helado) > 0:
@@ -12,7 +12,7 @@ while (hambre) < 85 and (dinero - precio_helado) > 0:
     if (hambre + edad) >=100:
         break
     print("Helados consumidos:" + str(helados_consumidos))
-    print("Dinero restante" + str(dinero))
+    print("Dinero restante:" + str(dinero))
     print("Nivel de saciedad:" +  str(hambre))
 
 
