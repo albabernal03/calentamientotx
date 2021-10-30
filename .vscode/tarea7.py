599334ad7c97cbf5ed6303d83e5f566cabcfde27
albabernal03 <ALBABERO@MYUAX.COM>
2021-10-30 20:27:33 +0200

    siguiente apartado del 7 hecho

--- .vscode/tarea7.py
+++ .vscode/tarea7.py
@@ -1,2 +1,27 @@
-for i in range(51):
+
+for i in range (51):
     print(i)
+    
+import random
+dinero = 2000
+edad = random.randint(0,50)
+hambre = edad
+precio_helado = 100
+helados_consumidos = 0
+
+
+while (hambre) < 85 and (dinero - precio_helado) > 0:
+    dinero = dinero - precio_helado
+    precio_helado = precio_helado + (precio_helado * 0.2)
+    helados_consumidos += 1
+    hambre = hambre + edad
+    if (hambre + edad) >=100:
+        break
+    print("Helados consumidos:" + str(helados_consumidos))
+    print("Dinero restante:" + str(dinero))
+    print("Nivel de saciedad:" +  str(hambre))
+
+
+
+
+
